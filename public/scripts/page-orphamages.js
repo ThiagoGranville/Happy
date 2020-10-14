a3840c55ba491309aee78976a5b6ad962a72f43d
Thiago Granville <thiagogranville@gmail.com>
2020-10-14 17:50:46 -0300

    Adicionada estilização do icone e popup do mapa, alterado local do mapa para Recife

--- public/scripts/page-orphamages.js
+++ public/scripts/page-orphamages.js
@@ -1,11 +1,26 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+// create map
+const map = L.map('mapid').setView([-8.0543953, -34.898379], 15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+// create and add tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-  .openPopup();
+// create icon
+const icon = L.icon({
+  iconUrl: './public/images/map-marker.svg',
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// create popup overlay
+const popup = L.popup({
+  closeButton: false,
+  className: 'map - popup',
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>'
+);
+
+// create and add marker
+L.marker([-8.0543953, -34.898379], { icon }).addTo(map).bindPopup(popup);
