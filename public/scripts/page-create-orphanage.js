2ba3850172edd33bb91ef2b21d2701969b4f69bf
Thiago Granville <thiagogranville@gmail.com>
2020-10-15 21:10:09 -0300

    Adicionada funcionalidade de escolher botão sim e não do formulario

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -30,45 +30,62 @@ map.on('click', (event) => {
 
 // add photo field
 function addPhotoField() {
-  //select images container
+  // select images container
   const container = document.querySelector('#images');
 
-  //select all upload fields
+  // select all upload fields
   const fieldsContainer = document.querySelectorAll('.new-upload');
 
-  //clone last upload field
+  // clone last upload field
   const newFieldContainer = fieldsContainer[
     fieldsContainer.length - 1
   ].cloneNode(true);
 
-  //verify if the upload field is empty
+  // verify if the upload field is empty
   const input = newFieldContainer.children[0];
 
   if (input.value == '') {
     return;
   }
 
-  //clean upload field value
+  // clean upload field value
   newFieldContainer.children[0].value = '';
 
-  //add a new upload field
+  // add a new upload field
   container.appendChild(newFieldContainer);
 }
 
-//remove photo field
+// remove photo field
 function deleteField(event) {
-  //get the current target
+  // get the current target
   const span = event.currentTarget;
 
-  //select all upload fields and verify if there is 1 or more
+  // select all upload fields and verify if there is 1 or more
   const fieldsContainer = document.querySelectorAll('.new-upload');
 
   if (fieldsContainer.length < 2) {
-    //clean upload field value
+    // clean upload field value
     span.parentNode.children[0].value = '';
     return;
   }
 
-  //delete a field
+  // delete a field
   span.parentNode.remove();
+}
+
+//select yes or no
+function toggleSelect(event) {
+  //remove .active from buttons
+  document
+    .querySelectorAll('.button-select button')
+    .forEach((button) => button.classList.remove('active'));
+
+  //add .active to this button
+  const button = event.currentTarget;
+  button.classList.add('active');
+
+  //update the hidden input value
+  const input = document.querySelector('[name="open_on_weekends"]');
+
+  input.value = button.dataset.value;
 }
