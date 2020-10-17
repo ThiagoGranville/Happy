99e27e58c84338d2614bef6904e7ecfd83bd922b
Thiago Granville <thiagogranville@gmail.com>
2020-10-16 21:05:36 -0300

    adicionado fakedata para teste

---
+++ src/database/fakedata.js
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    id: 1,
+    lat: '-8.0543953',
+    lng: '-34.898379',
+    name: 'Lar das meninas',
+    description:
+      'Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.',
+    images: [
+      'https://images.unsplash.com/photo-1598454444427-8bffa498b6f2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+      'https://images.unsplash.com/photo-1572058685927-5175f7320c90?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+      'https://images.unsplash.com/photo-1598137203989-3152bec01cf4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+    ],
+    instruction:
+      'Venha se sentir a vontade e traga muito amor e paciência para dar.',
+    opening_hours: 'Horário de visitas das 8h até 18h',
+    open_on_weekends: '1',
+  },
+  {
+    id: 2,
+    lat: '-8.0543953',
+    lng: '-34.868379',
+    name: 'Lar dos meninos',
+    description:
+      'Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.',
+    images: [
+      'https://images.unsplash.com/photo-1598454444427-8bffa498b6f2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+      'https://images.unsplash.com/photo-1572058685927-5175f7320c90?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+      'https://images.unsplash.com/photo-1598137203989-3152bec01cf4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+    ],
+    instruction:
+      'Venha se sentir a vontade e traga muito amor e paciência para dar.',
+    opening_hours: 'Horário de visitas das 8h até 18h',
+    open_on_weekends: '0',
+  },
+];
