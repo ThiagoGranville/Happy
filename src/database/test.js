9e3f904603bdb27f74be3f71acdf3f84260ddd78
Thiago Granville <thiagogranville@gmail.com>
2020-10-17 18:03:08 -0300

    Alteração para dados dinâmicos do nome do orfanato, descrição, instruções, localização e informações

--- src/database/test.js
+++ src/database/test.js
@@ -7,7 +7,7 @@ database.then(async db => {
   await saveOrphanage(db, {
     lat: '-8.0543953',
     lng: '-34.868379',
-    name: 'Lar das meninos',
+    name: 'Lar dos meninos',
     about:
       'Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.',
     whatsapp: '9829292929',
@@ -16,7 +16,7 @@ database.then(async db => {
       'https://images.unsplash.com/photo-1572058685927-5175f7320c90?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
       'https://images.unsplash.com/photo-1598137203989-3152bec01cf4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
     ].toString(),
-    instruction:
+    instructions:
       'Venha se sentir a vontade e traga muito amor e paciência para dar.',
     opening_hours: 'Horário de visitas das 8h até 18h',
     open_on_weekends: '0'
@@ -27,8 +27,8 @@ database.then(async db => {
   console.log(selectedOrphanages);
 
   // //consultar somente 1 orfanato pelo ID
-  const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"');
-  console.log(orphanage);
+  // const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"');
+  // console.log(orphanage);
 
   // //deletar um dano
   // console.log(await db.run("DELETE FROM orphanages WHERE id = '4'"));
--- src/pages.js
+++ src/pages.js
@@ -1,16 +1,46 @@
-const orphanages = require('./database/fakedata.js');
+const database = require('./database/db');
+const saveOrphanage = require('./database/saveOrphanage');
 
 module.exports = {
   index(req, res) {
     return res.render('index');
   },
 
-  orphanage(req, res) {
-    return res.render('orphanage');
+  async orphanage(req, res) {
+    try {
+      const id = req.query.id;
+
+      const db = await database;
+      const results = await db.all(
+        `SELECT * FROM orphanages WHERE id = "${id}"`
+      );
+      const orphanage = results[0]
+
+      orphanage.images = orphanage.images.split(",")
+      orphanage.firstImage = orphanage.images[0];
+      
+      if(orphanage.open_on_weekends == "0"){
+        orphanage.open_on_weekends = false;
+      }else{
+        orphanage.open_on_weekends = true;
+      }
+
+      return res.render('orphanage', {orphanage});
+    } catch (error) {
+      console.log(error);
+      return res.send('Erro no banco de dados!');
+    }
   },
 
-  orphanages(req, res) {
-    return res.render('orphanages', { orphanages });
+  async orphanages(req, res) {
+    try {
+      const db = await database;
+      const orphanages = await db.all('SELECT * FROM orphanages');
+      return res.render('orphanages', { orphanages });
+    } catch (error) {
+      console.log(error);
+      return res.send('Erro no banco de dados!');
+    }
   },
 
   createOrphanage(req, res) {
