7b962e53c1dff3d2b7156fb5387c0bf96e68b7f2
Thiago Granville <thiagogranville@gmail.com>
2020-10-16 22:49:49 -0300

    Função para criar marcadores no mapa com base na fake data adicionado ao javascript da page-orphanages

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,26 +1,48 @@
 // create map
-const map = L.map('mapid').setView([-8.0543953, -34.898379], 15);
+const map = L.map("mapid").setView([-8.0543953, -34.898379], 15);
 
 // create and add tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // create icon
 const icon = L.icon({
-  iconUrl: '/images/map-marker.svg',
+  iconUrl: "/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [170, 2],
 });
 
-// create popup overlay
-const popup = L.popup({
-  closeButton: false,
-  className: 'map - popup',
-  minWidth: 240,
-  minHeight: 240,
-}).setContent(
-  'Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage"><img src="/images/arrow-white.svg"></a>'
-);
 
-// create and add marker
-L.marker([-8.0543953, -34.898379], { icon }).addTo(map).bindPopup(popup);
+function addMarker({id, name, lat, lng}) {
+
+  // create popup overlay
+  const popup = L.popup({
+    closeButton: false,
+    className: "map - popup",
+    minWidth: 240,
+    minHeight: 240,
+  }).setContent(
+    `${name} <a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg"></a>`
+  );
+
+  // create and add marker
+  L
+  .marker([lat, lng], { icon })
+  .addTo(map)
+  .bindPopup(popup);
+}
+
+const orphanageSpan = document.querySelectorAll('.orphanages span')
+orphanageSpan.forEach( span => {
+
+  const orphanage = {
+
+    id: span.dataset.id,
+    name: span.dataset.name,
+    lat: span.dataset.lat,
+    lng: span.dataset.lng
+
+  }
+
+  addMarker(orphanage);
+})
